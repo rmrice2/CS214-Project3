239417a828d5ab5012af092ad3cf30b134ce97c0
yequsun <yequsun@gmail.com>
2016-12-14 19:00:28 -0500

    header update for extension A
    added a few structs and functions

--- libnetfiles.h
+++ libnetfiles.h
@@ -10,6 +10,9 @@ o - open
 r - read
 w - write
 */
+#define UNRESTRICTED 0;
+#define EXCLUSIVE 1;
+#define TRANSACTION 2;
 
 typedef struct int_packet{
 	char type;
@@ -23,6 +26,35 @@ typedef struct client{
 	socklen_t addr_size;
 } Client;
 
+//for extension A
+typedef struct client_node{
+	int fd;//file desciptor
+	int file_mode;//unrestricted exclusive or transaction
+	int read; //if it has read permission
+	int write; //if it has write permission
+	client_node* prev;
+	client_node* next;
+};
+
+typedef struct file_node{
+	char* path;//full file path with name
+	client_node* user;
+	client_node* prev;
+	file_node* next;
+	//flags
+	int exists_client; //this file node has at least one user
+	int exists_trans; //exists a user in transction mode
+	int exists_write; // exits a user with write permission
+	int exists_exclusivew; //exists a user in exclusive mode with write permission
+};
+
+typedef struct query{ 
+  int fd; //file descriptor 
+  char op; //operatons: o-open r-read w-write c-close 
+  char flag; //read write or both 
+  int file_mode;//modes in extension A 
+  char* msg; //usually is file path 
+}; 
 int netopen(const char *, int);
 ssize_t netread(int, void *, size_t);
 ssize_t netwrite(int, const void *, size_t);
@@ -35,5 +67,15 @@ void handle_read(Client, char *);
 void handle_write(Client, char *);
 void handle_close(int, char *);
 int check_socks();
+
+
+//functions for extension A
+file_node get_file_node(file_node head);//check if the file is already existed. return NULL on failure
+void update_file_node(file_node fn); //check the permission and modes of all clients to set the flags
+void insert_file_node(file_node head,char* path);//create a new file node and put it in the list
+void delete_file_node(file_node fn);//delete a file node
+
+void insert_client_node(client_node cn,query q);
+void delete_client_node(client_node cn);
 
 #endif
